6f39bbcd78fcabd50c705625380ebf8c595b5e2e
alexissengangabo07 <alexxngabo@gmail.com>
2022-08-10 23:15:52 +0200

    card structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { AiOutlineMail, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import { SiTwitter, SiInstagram } from 'react-icons/si'
+import { ImFacebook } from 'react-icons/im'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div>
+        <div>
+          <img src='./images/photo.jpg' alt="user" />
+        </div>
+        <div>
+          <h1>Laura Smith</h1>
+          <h2>Frontend Developer</h2>
+          <h5>laurasmith.website</h5>
+        </div>
+        <div>
+          <div><button><AiOutlineMail /> Email</button></div>
+          <div><button><AiFillLinkedin /> Linkedin</button></div>
+        </div>
+        <div>
+          <div>
+            <h2>About</h2>
+            <div>
+              <p>
+                I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+              </p>
+            </div>
+            <h2>Interests</h2>
+            <div>
+              <p>
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button><SiTwitter /></button>
+        <button><ImFacebook /></button>
+        <button><SiInstagram /></button>
+        <button><AiFillGithub /></button>
+      </div>
     </div>
   );
 }
